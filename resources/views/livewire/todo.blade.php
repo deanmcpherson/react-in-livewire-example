734cfe2dfee72f91ee5735bd2f31b543aff5fd39
Dean McPherson <dean@paperform.co>
2024-05-25 08:52:15 +1000

    feat: tidy up

--- resources/views/livewire/todo.blade.php
+++ resources/views/livewire/todo.blade.php
@@ -8,19 +8,15 @@ state([
 ]);
 
 $addTask = function($taskText) {
-    $task = [
+    $this->tasks[] = [
         'id' => Str::random(10),
         'text' => $taskText
-];
-    $this->tasks[] = $task;
-};
+];};
 
 $removeTask = function($taskId) {
-
     $tasks = collect($this->tasks)->filter(function($task) use ($taskId) {
         return $task['id'] !== $taskId;
     })->values()->toArray();
-
     $this->tasks = $tasks;
 };
 
@@ -30,18 +26,14 @@ function render({wire: {tasks, addTask, removeTask}}) {
     return <div className=" border p-4 rounded-lg mt-2">
             <ul>{
                 tasks?.map(task => {
-                   return <li className="w-full flex">{task.text} <button className="ml-auto" onClick={() => {
-                        removeTask(task.id)
-                    }}>x</button></li>
+                   return <li className="w-full flex">
+                    {task.text}
+                    <button className="ml-auto" onClick={() => removeTask(task.id)}>x</button>
+                </li>
                 })
             }
             </ul>
-            {
-                !tasks.length && <div>No tasks</div>
-            }
-            <input type="text"
-            className="bg-white h-8 text-black rounded border-none w-full"
-            placeholder="Add Todo"
+            <input type="text" className="bg-white h-8 text-black rounded border-none w-full" placeholder="Add Todo"
             onKeyPress={(e) => {
                 if (e.key === 'Enter' && e.target.value) {
                     addTask(e.target.value)
